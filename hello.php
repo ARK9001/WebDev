642bb877891bd8ae77e3f0b7a7e6fd4b25a36a50
ARK9001 <aish2012@gmail.com>
2016-08-25 23:00:14 -0400

    Using forms and query with php

--- hello.php
+++ hello.php
@@ -5,24 +5,11 @@
  <body>
  
 
-<?php
-echo $_SERVER['HTTP_USER_AGENT'];
-?>
-
-<?php
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
-?>
-<h3>strpos() must have returned non-false</h3>
-<p>You are using Internet Explorer</p>
-<?php
-} else {
-?>
-<h3>strpos() must have returned false</h3>
-<p>You are not using Internet Explorer</p>
-<?php
-}
-?>
-
+<form action="action.php" method="post">
+ <p>Your name: <input type="text" name="name" /></p>
+ <p>Your age: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
 
 <?php phpinfo(); ?>
 
